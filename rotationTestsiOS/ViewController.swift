7b972b4ddf16a4eafd235dd10cfcb46b5549a243
Tom <tom.a.fischer@gmail.com>
2019-05-14 14:33:14 -0400

    Initial Commit

---
+++ rotationTestsiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rotationTestsiOS
+//
+//  Created by Fischer, Thomas Alfons on 5/14/19.
+//  Copyright © 2019 aft3000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
